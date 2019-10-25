f712fea732a75675aabe9fbb0e7d9d9f3f063ff0
Jennifer Batara <jennifer.batara.dev@gmail.com>
2019-10-25 14:46:59 -0700

    Add new, edit, and detail routes to Flavor controller

--- SweetNSavory/Controllers/FlavorController.cs
+++ SweetNSavory/Controllers/FlavorController.cs
@@ -7,13 +7,14 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using SweetNSavory.Data;
 using SweetNSavory.Models;
 
 namespace SweetNSavory.Controllers {
-    [Authorize]
-    public class FlavorController : Controller {
+  [Authorize]
+  public class FlavorController : Controller {
     private readonly ILogger<HomeController> _logger;
     private readonly ApplicationDbContext _db;
     private readonly UserManager<ApplicationUser> _userManager;
@@ -24,8 +25,58 @@ namespace SweetNSavory.Controllers {
       _userManager = userManager;
     }
 
-    public IActionResult Index () {
+    public async Task<ActionResult> Index () {
+      var userId = this.User.FindFirst (ClaimTypes.NameIdentifier)?.Value;
+      var currentUser = await _userManager.FindByIdAsync (userId);
+      var userFlavors = _db.Flavors.Where (entry => entry.User.Id == currentUser.Id);
+      return View (userFlavors);
+    }
+
+    [HttpGet]
+    public async Task<ActionResult> New () {
+      var userId = this.User.FindFirst (ClaimTypes.NameIdentifier)?.Value;
+      var currentUser = await _userManager.FindByIdAsync (userId);
       return View ();
+    }
+
+    [HttpPost]
+    public async Task<ActionResult> New (Flavor f) {
+      var userId = this.User.FindFirst (ClaimTypes.NameIdentifier)?.Value;
+      var currentUser = await _userManager.FindByIdAsync (userId);
+      f.User = currentUser;
+      _db.Add (f);
+      _db.SaveChanges ();
+      return RedirectToAction ("Index", "Treat");
+    }
+
+    [HttpGet]
+    public async Task<ActionResult> Details (int id) {
+      var userId = this.User.FindFirst (ClaimTypes.NameIdentifier)?.Value;
+      var currentUser = await _userManager.FindByIdAsync (userId);
+      var model = _db.Flavors.FirstOrDefault (t => t.FlavorId == id);
+      return View (model);
+    }
+
+    [HttpGet]
+    public async Task<ActionResult> Edit (int id) {
+      var userId = this.User.FindFirst (ClaimTypes.NameIdentifier)?.Value;
+      var currentUser = await _userManager.FindByIdAsync (userId);
+      var model = _db.Flavors.FirstOrDefault (t => (t.FlavorId == id) & (t.User.Id == currentUser.Id));
+      ViewBag.Flavors = _db.Flavors.ToList ();
+      ViewBag.FlavorId = id;
+      return View (model);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult> Edit (Flavor f, int treatId) {
+      var userId = this.User.FindFirst (ClaimTypes.NameIdentifier)?.Value;
+      var currentUser = await _userManager.FindByIdAsync (userId);
+      if (treatId != 0) {
+        _db.TreatFlavors.Add (new TreatFlavor () { TreatId = treatId, FlavorId = treatId });
+      }
+      _db.Entry (f).State = EntityState.Modified;
+      _db.SaveChanges ();
+      return RedirectToAction ("Detail", "Treat", new { id = f.FlavorId });
     }
 
     public IActionResult Privacy () {
