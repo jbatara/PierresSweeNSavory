568f93fd840e6fdc5fdd400eaa4289eb67e7db71
Jennifer Batara <jennifer.batara.dev@gmail.com>
2019-10-25 11:29:03 -0700

    Add Migration change to database

--- SweetNSavory/Data/Migrations/20191025181637_IdentityUserToFlavor.Designer.cs
+++ SweetNSavory/Data/Migrations/20191025181637_IdentityUserToFlavor.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SweetNSavory.Data;
 
 namespace SweetNSavory.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20191025181637_IdentityUserToFlavor")]
+    partial class IdentityUserToFlavor
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
