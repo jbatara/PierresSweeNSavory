bccc1011dec94f1ad35a4f883aa4a10f5a4c21a6
Jennifer Batara <jennifer.batara.dev@gmail.com>
2019-10-25 12:15:39 -0700

    Add New treat route in controller

--- SweetNSavory/Controllers/TreatController.cs
+++ SweetNSavory/Controllers/TreatController.cs
@@ -28,8 +28,27 @@ namespace SweetNSavory.Controllers {
       var userId = this.User.FindFirst (ClaimTypes.NameIdentifier)?.Value;
       var currentUser = await _userManager.FindByIdAsync (userId);
       var userTreats = _db.Treats.Where (entry => entry.User.Id == currentUser.Id);
-      return View(userTreats);
+      return View (userTreats);
     }
+
+    [HttpGet]
+    public async Task<ActionResult> New () {
+      var userId = this.User.FindFirst (ClaimTypes.NameIdentifier)?.Value;
+      var currentUser = await _userManager.FindByIdAsync (userId);
+      return View ();
+    }
+
+    [HttpPost]
+    public async Task<ActionResult> New (Treat t) {
+      var userId = this.User.FindFirst (ClaimTypes.NameIdentifier)?.Value;
+      var currentUser = await _userManager.FindByIdAsync (userId);
+      t.User = currentUser;
+      _db.Add (t);
+      _db.SaveChanges ();
+      return RedirectToAction ("Index", "Treat");
+    }
+
+    
 
     public IActionResult Privacy () {
       return View ();
