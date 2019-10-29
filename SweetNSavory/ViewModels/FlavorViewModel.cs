8e608a71afd0b8b2be9f0c08e0b8a939f36c6a53
Jennifer Batara <jennifer.batara.dev@gmail.com>
2019-10-28 23:38:59 -0700

    Add FlavorViewModel and Flavor index view

---
+++ SweetNSavory/ViewModels/FlavorViewModel.cs
@@ -0,0 +1,7 @@
+using System.Collections.Generic;
+namespace SweetNSavory.Models {
+  public class FlavorViewModel {
+    public Flavor Flavor { get; set; }
+    public List<Treat> Treats { get; set; }
+  }
+}
