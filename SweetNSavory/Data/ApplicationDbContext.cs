0cb81f03bb17895c0f9486f2585cabaed043a935
Jennifer Batara <jennifer.batara.dev@gmail.com>
2019-10-30 00:15:27 -0700

    WIP

--- SweetNSavory/Data/ApplicationDbContext.cs
+++ SweetNSavory/Data/ApplicationDbContext.cs
@@ -1,13 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using SweetNSavory.Models;
 
 namespace SweetNSavory.Data
 {
-    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    public class ApplicationDbContext : IdentityDbContext<IdentityUser>
     {
         public DbSet<Treat> Treats {get;set;}
         public DbSet<Flavor> Flavors {get;set;}
