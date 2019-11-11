a68cca073c070a44fec7b456cd416e8ebc55c004
Jennifer Batara <jennifer.batara.dev@gmail.com>
2019-11-11 14:08:56 -0800

    Make dropdownlist methods for Treat and Flavor

--- SweetNSavory/Models/Flavor.cs
+++ SweetNSavory/Models/Flavor.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc.Rendering;
 namespace SweetNSavory.Models {
   public class Flavor {
     public Flavor () {
@@ -8,5 +9,22 @@ namespace SweetNSavory.Models {
     public string FlavorName { get; set; }
     public virtual ApplicationUser User { get; set; }
     public ICollection<TreatFlavor> TreatFlavors { get; set; }
+
+
+    public SelectListItem ToDropDownItem()
+    {
+      SelectListItem item = new SelectListItem { Text = $"{FlavorName}", Value = $"{ FlavorId }" };
+      return item;
+    }
+
+    public static List<SelectListItem> ListToDropDownItems(List<Flavor> flavors)
+    {
+      List<SelectListItem> list = new List<SelectListItem>();
+      foreach (Flavor f in flavors)
+      {
+        list.Add(f.ToDropDownItem());
+      }
+      return list;
+    }
   }
 }
--- SweetNSavory/Models/Treat.cs
+++ SweetNSavory/Models/Treat.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc.Rendering;
 namespace SweetNSavory.Models
 {
   public class Treat
@@ -9,7 +10,23 @@ namespace SweetNSavory.Models
     }
     public int TreatId { get; set; }
     public string TreatName { get; set; }
-    public virtual ApplicationUser User {get;set;}
+    public virtual ApplicationUser User { get; set; }
     public ICollection<TreatFlavor> TreatFlavors { get; set; }
+
+    public SelectListItem ToDropDownItem()
+    {
+      SelectListItem item = new SelectListItem { Text = $"{TreatName}", Value = $"{ TreatId }" };
+      return item;
+    }
+
+    public static List<SelectListItem> ListToDropDownItems(List<Treat> treats)
+    {
+      List<SelectListItem> list = new List<SelectListItem>();
+      foreach (Treat t in treats)
+      {
+        list.Add(t.ToDropDownItem());
+      }
+      return list;
+    }
   }
 }
